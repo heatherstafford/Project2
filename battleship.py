189a422d593bac07765e6d9e76b2e772fff05eea
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-23 08:39:43 -0400

    Updated from Brython Server: 5/23/2018 8:39:41 AM

--- battleship.py
+++ battleship.py
@@ -1,1 +1,11 @@
+#Heather Stafford
+#5/23/18
+#battleship.py - project2
+
+from ggame import *
+
+def buildBoard():
+    for r in range(0,5):
+        for c in range(0,5):
+            board[r][c]
 
