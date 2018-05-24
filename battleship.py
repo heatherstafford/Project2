5adb2c26d3cddb1ccbd868665fa0cdec3c2d57f4
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-24 08:32:35 -0400

    Updated from Brython Server: 5/24/2018 8:32:35 AM

--- battleship.py
+++ battleship.py
@@ -4,7 +4,7 @@
 
 from ggame import *
 
-RADIUS = 15
+RADIUS = 30
 
 white = Color(0xFFFFFF,1)
 black = Color(0x000000,1)
@@ -15,9 +15,9 @@ def buildBoard():
 def redrawAll():
     for item in App().spritelist[:]:
         item.destroy()
-    for r in range(5):
-        for c in range(5):
-            Sprite(boardgraphics(2*RADIUS+10)*r,(2*RADIUS+10)*c))
+    for r in range(0,5):
+        for c in range(0,5):
+            Sprite(boardgraphics)
 
 if __name__ == '__main__': 
     
