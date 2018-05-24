2e909dd399bd3efaba33aad0222b78dbff14269b
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-05-24 08:11:02 -0400

    Updated from Brython Server: 5/24/2018 8:11:02 AM

--- battleship.py
+++ battleship.py
@@ -5,7 +5,18 @@
 from ggame import *
 
 def buildBoard():
-    for r in range(0,5):
-        for c in range(0,5):
-            board[r][c]
+    for r in range(0,3):
+        for c in range(0,3):
+            print(board[r][c], end=' ')
+        print()
 
+board = [['','',''],['','',''],['','','']]
+            
+def redrawAll():
+    for item in App().spritelist[:]:
+        item.destroy()
+
+if __name__ == '__main__': 
+    
+    App().run()
+    
