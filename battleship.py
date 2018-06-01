0af11846e8fb2dab4334c99f8c94abef804f2d03
heatherstafford <35492769+heatherstafford@users.noreply.github.com>
2018-06-01 08:37:07 -0400

    Updated from Brython Server: 6/1/2018 8:37:06 AM

--- battleship.py
+++ battleship.py
@@ -9,7 +9,7 @@ RADIUS = 50
 
 white = Color(0xFFFFFF,1)
 black = Color(0x000000,1)
-blue = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
 
 def buildBoard():
     return[['','','','',''],['','','','',''],['','','','',''],['','','','',''],['','','','','']]
